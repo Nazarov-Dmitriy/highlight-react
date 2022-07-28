5f3af797b3e7b344214669e5aa69ff835188b702
Nazarov Dmitry <Difat1@inbox.ru>
2022-07-28 15:09:30 +0300

    rework

--- src/components/List.js
+++ src/components/List.js
@@ -1,66 +1,21 @@
 import React from "react";
 import { nanoid } from "nanoid";
+import Video from "./Video";
+import Article from "./Article";
+import Popular from "./Popular";
+import New from "./New";
 
 export default function List(props) {
-  function withData() {
-    return function () {
-      const Func = function (props) {
-        function Component1() {
-          return props.views < 100 ? props.children[1] : props.children[2];
-        }
-        return <Component1 />;
-      };
-
-      return Func;
+  function withHoc() {
+    return function Wrapper(props) {
+      function Component1() {
+        return props.views < 100 ? props.children[1] : props.children[2];
+      }
+      return <Component1 />;
     };
   }
 
-  const ListItem = withData()();
-
-  function New(props) {
-    return (
-      <div className="wrap-item wrap-item-new">
-        <span className="label">New!</span>
-        {props.children}
-      </div>
-    );
-  }
-
-  function Popular(props) {
-    return (
-      <div className="wrap-item wrap-item-popular">
-        <span className="label">Popular!</span>
-        {props.children}
-      </div>
-    );
-  }
-
-  function Article(props) {
-    return (
-      <div className="item item-article">
-        <h3>
-          <a href="##">{props.title}</a>
-        </h3>
-        <p className="views">Прочтений: {props.views} </p>
-      </div>
-    );
-  }
-
-  function Video(props) {
-    return (
-      <div className="item item-video">
-        ssssssss
-        <iframe
-          title="props.url"
-          src={props.url}
-          frameBorder="0"
-          allow="autoplay; encrypted-media"
-          allowFullScreen
-        ></iframe>
-        <p className="views">Просмотров: {props.views}</p>
-      </div>
-    );
-  }
+  const ListItem = withHoc();
 
   function List(props) {
     return props.list.map((item) => {
